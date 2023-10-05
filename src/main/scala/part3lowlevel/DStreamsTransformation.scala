0b627601fec53e9244bc40d41a3493a6afe57e52
Marcin Szaroszyk <marcin.szaroszyk@gmail.com>
2023-10-05 21:41:35 +0200

    60m

--- src/main/scala/part3lowlevel/DStreamsTransformation.scala
+++ src/main/scala/part3lowlevel/DStreamsTransformation.scala
@@ -2,9 +2,12 @@ package part3lowlevel
 
 import common._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+import java.io.File
 import java.sql.Date
+import java.time.{LocalDate, Period}
 
 object DStreamsTransformation {
 
@@ -15,7 +18,8 @@ object DStreamsTransformation {
 
   val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
 
-  def readPeople() = ssc.socketTextStream("localhost", 12345).map { line =>
+import spark.implicits._
+  def readPeople(): DStream[Person] = ssc.socketTextStream("localhost", 12345).map { line =>
     val tokens = line.split(":")
     Person(
       tokens(0).toInt,
@@ -29,10 +33,44 @@ object DStreamsTransformation {
     )
   }
 
+  // map, flatMap, filter
+  def peopleAges(): DStream[(String, Int)] = readPeople().map { person =>
+    val age = Period.between(person.birthDate.toLocalDate, LocalDate.now()).getYears
+    (s"${person.firstName} ${person.lastName}", age)
+  }
+
+  def peopleSmallNames(): DStream[String] = readPeople().flatMap { person =>
+    List(person.firstName, person.middleName)
+  }
+
+  def highIncomePeople() = readPeople().filter(_.salary > 80000)
+
+  def countPeople() = readPeople().count()
+
+  def countNames(): DStream[(String, Long)] = readPeople().map(_.firstName).countByValue()
+
+  def countNamesReduce(): DStream[(String, Int)] =
+    readPeople()
+      .map(_.firstName)
+      .map(name => (name, 1))
+      .reduceByKey((a, b) => a + b)
+
+  def saveToJson() = readPeople()
+    .foreachRDD { rdd =>
+      val ds = spark.createDataset(rdd)
+      val f = new File("src/main/resources/data/people")
+      val nFiles = f.listFiles().length
+      val path = s"src/main/resources/data/people/people$nFiles.json"
+
+      ds.write.json(path)
+    }
+
 
   def main(args: Array[String]): Unit = {
-    val stream = readPeople()
-    stream.print()
+//    val stream = saveToJson()
+//    stream.print()
+    saveToJson()
+
     ssc.start()
     ssc.awaitTermination()
   }
