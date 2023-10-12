567c90b56af430fed2744052bb336e9c15146836
Marcin Szaroszyk <marcin.szaroszyk@gmail.com>
2023-10-12 21:36:33 +0200

    30m

--- src/main/scala/part3lowlevel/DStreamsWindowTransformation.scala
+++ src/main/scala/part3lowlevel/DStreamsWindowTransformation.scala
@@ -21,8 +21,74 @@ object DStreamsWindowTransformation {
 
   def linesByWindowCount() = readLines().window(Seconds(10)).count()
 
+  def sumAllTextByWindow() = readLines()
+    .map(_.length)
+    .window(Seconds(10), Seconds(5))
+    .reduce(_ + _)
+
+  def sumAllTextByWindowAlt() = readLines()
+    .map(_.length)
+    .reduceByWindow(_ + _, Seconds(10), Seconds(5))
+
+  def linesByTumblingWindow() = readLines()
+    .window(Seconds(10), Seconds(10))
+
+  def computeWordOcurrences() = {
+
+    // for reducebykeyandwindow i need checkpoint directory set
+    ssc.checkpoint("checkpoints")
+
+    readLines()
+      .flatMap(_.split(" "))
+      .map(word => (word, 1))
+      .reduceByKeyAndWindow(
+        (a, b) => a + b, // reduction function
+        (a, b) => a - b, // "inverse" function
+        Seconds(60), // window duration
+        Seconds(30) // sliding duration
+      )
+  }
+
+  val moneyPerExpensiveWord = 2
+
+  def showMeTheMoney() = readLines()
+    .flatMap(line => line.split(" "))
+      .filter(_.length >= 10)
+      .map(_ => moneyPerExpensiveWord)
+      .reduce(_ + _)
+      .window(Seconds(30), Seconds(10))
+      .reduce(_ + _)
+
+  def showMeTheMoney2() = readLines()
+    .flatMap(line => line.split(" "))
+    .filter(_.length >= 10)
+    .countByWindow(Seconds(30), Seconds(10))
+    .map(_ * moneyPerExpensiveWord)
+
+  def showMeTheMoney3() = readLines()
+    .flatMap(line => line.split(" "))
+    .filter(_.length >= 10)
+    .map(_ => moneyPerExpensiveWord)
+    .reduceByWindow(_ + _, Seconds(30), Seconds(10))
+
+  def showMeTheMoney4() = {
+
+    ssc.checkpoint("checkpoints")
+
+    readLines()
+      .flatMap(line => line.split(" "))
+      .filter(_.length >= 10)
+      .map { word =>
+        if (word.length >= 10) ("expensive", 2)
+        else ("cheap", 1)
+      }
+      .reduceByKeyAndWindow(_ + _,_ - _, Seconds(30), Seconds(10))
+  }
+
+
+
   def main(args: Array[String]): Unit = {
-    linesByWindowCount.print()
+    computeWordOcurrences.print()
     ssc.start()
     ssc.awaitTermination()
   }
