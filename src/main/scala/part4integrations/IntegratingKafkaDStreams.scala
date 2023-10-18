ae61ba38def7070c4f0685e87c250502794784ad
Marcin Szaroszyk <marcin.szaroszyk@gmail.com>
2023-10-18 21:58:34 +0200

    15m

--- src/main/scala/part4integrations/IntegratingKafkaDStreams.scala
+++ src/main/scala/part4integrations/IntegratingKafkaDStreams.scala
@@ -1,5 +1,53 @@
 package part4integrations
 
+import java.util
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 object IntegratingKafkaDStreams {
+
+  val spark = SparkSession.builder()
+    .appName("Integrating Kafka")
+    .master("local[*]")
+    .getOrCreate()
+
+  val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
+
+  val kafkaParams: Map[String, Object] = Map(
+    "bootstrap.servers" -> "localhost:9092",
+    "key.serializer" -> classOf[StringSerializer],
+    "value.serializer" -> classOf[StringSerializer],
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> false.asInstanceOf[Object]
+  )
+
+  val kafkaTopic = "rockthejvm"
+
+  def readFromKafka() = {
+    val topics = Array(kafkaTopic)
+    val kafkaDStream = KafkaUtils.createDirectStream(
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](topics, kafkaParams + ("group.id" -> "group1"))
+    )
+
+    val processedStream = kafkaDStream.map(record => (record.key(), record.value()))
+    processedStream.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+
+
+  def main(args: Array[String]): Unit = {
+    readFromKafka()
+  }
 
 }
